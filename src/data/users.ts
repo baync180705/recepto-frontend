ae0bb9b94af4f0c481e6b1b46c836225fe42786c
Budhayan Chakraborty <budhayanc2005@gmail.com>
2025-04-03 11:36:09 +0530

    refactor: Rename src/models to src/types, Move hard-coded dummy data to src/data | fix: Remove unused Login page

---
+++ src/data/users.ts
@@ -0,0 +1,9 @@
+const users = [
+    { username: "user1", password: "test123" },
+    { username: "user2", password: "pass123" },
+    { username: "user3", password: "demo123" },
+    { username: "user4", password: "hello123" },
+    { username: "user5", password: "world123" },
+];
+
+export default users;
